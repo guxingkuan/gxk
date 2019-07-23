a99f2799d0b181613b86449fa914ff3d6b10755e
guxingkuan <52728345+guxingkuan@users.noreply.github.com>
2019-07-23 08:45:38 +0800

    Add files via upload

---
+++ 15.php
@@ -0,0 +1,33 @@
+<?php 	
+
+$arr  = 'f';
+
+$str = ("ssdffsgdfaqrewgfdfaewegsdsdwretwrfsvsdrqergfzsdwrqewtrsgfdzfsdqtert4etgfsdfqr2t4eth");
+
+function Number($arr,$str)
+{
+	$length = length($str);
+
+	for ($i=0; $i < $length; $i++) 
+	{ 
+		return $n = strpos($arr,$str);
+	}
+
+	
+}
+
+
+var_dump($n);
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
