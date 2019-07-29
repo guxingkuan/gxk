363f9b9fe64ede07585be96f0ed460cd72b9045d
guxingkuan <52728345+guxingkuan@users.noreply.github.com>
2019-07-29 08:48:52 +0800

    Add files via upload

---
+++ 20.php
@@ -0,0 +1,35 @@
+<?php 
+$x = 20;
+$y = 30;
+
+$str = '';
+
+function CountSteps($x,$y)
+{
+	for ($i=0; $i < $x ; $i++) { 
+		for ($j=0; $j <$y ; $i++) { 
+			echo $str =  $i+$j;
+		}
+	}
+}
+
+
+s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
